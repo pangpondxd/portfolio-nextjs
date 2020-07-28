4bfe29c06fd075a284082dfe419654fe37b9a713
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-07-28 13:04:41 +0700

    add portfolio css

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ class Index extends React.Component {
                 <Col md="6" className="hero-welcome-wrapper">
                   <div className="hero-welcome-text">
                     <h1>
-                      Welcome to the portfolio website of Filip Jerga. Get
+                      Welcome to the portfolio website of Tanawat Wirattangsakul. Get
                       informed, collaborate and discover projects I was working
                       on through the years!
                     </h1>
